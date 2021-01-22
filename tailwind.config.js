75d0c24e0918421b58efe70e096e08003558516e
Dang Dinh Sang <sang.dang.connectiv@gmail.com>
2021-01-22 19:19:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.vue'] },
+    darkMode: false,
+    // or 'media' or 'class'
+    theme: { extend: {},
+        colors: {}
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
